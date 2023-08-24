be3a75606a6961f4c9396bab9737943e88a3d021
alexkimani <joealex.kimani@gmail.com>
2023-08-24 15:05:51 +0300

    Create initial project structure.

---
+++ src/test/java/com/logicea/cardsapi/CardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logicea.cardsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
